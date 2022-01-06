bb063af8b4b2e93862a53a52e4150d66932afbef
arpitmh25 <95077786+arpitmh25@users.noreply.github.com>
2022-01-06 08:56:38 +0530

    Update DeckOfCards.java

--- src/com/bridgelabz/workshop2/DeckOfCards.java
+++ src/com/bridgelabz/workshop2/DeckOfCards.java
@@ -1,16 +1,29 @@
 package com.bridgelabz.workshop2;
 
+import java.util.ArrayList;
 
 public class DeckOfCards {
+    public ArrayList<String> cardsDeck = new ArrayList<>();
 
-    static String[] hearts = {"H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "H10", "HJack", "HQueen", "HKing", "HAce"};
 
-    static String[] diamonds = {"D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "D10", "DJack", "DQueen", "DKing", "DAce"};
+    public void deckofCards() {
 
-    static String[] clubs = {"C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "CJack", "CQueen", "CKing", "CAce"};
+        String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
+        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "King", "Queen", "Ace"};
+        int numOfCards = suits.length * ranks.length;
+        System.out.println("\nNumber of cards in the deck:" + numOfCards);
+        for (int i = 0; i < ranks.length; i++) {
+            for (int j = 0; j < suits.length; j++) {
+                cardsDeck.add(ranks[i] + "->" + suits[j]);
+            }
+        }
+        toDisplay(cardsDeck);
+    }
 
-    static String[] spades = {"S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "S10", "SJack", "SQueen", "SKing", "SAce"};
-
-    static String[][] deckOfCards = {hearts, diamonds, clubs, spades};
-    static String[][] newDeck;
-
+    public static void toDisplay(ArrayList<String> cardsDeck) {
+        for (String element : cardsDeck) {
+            System.out.println(element);
+        }
+        System.out.println();
+    }
+}
