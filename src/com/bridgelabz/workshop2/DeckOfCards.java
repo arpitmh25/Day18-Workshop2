c8c407b7a9897c793f7f2b69f1c4e0173dcc8e5a
Arpit <arpitmh23@gmail.com>
2022-01-06 08:38:55 +0530

    Adding use case 1

---
+++ src/com/bridgelabz/workshop2/DeckOfCards.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.workshop2;
+
+
+public class DeckOfCards {
+
+    static String[] hearts = {"H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "H10", "HJack", "HQueen", "HKing", "HAce"};
+
+    static String[] diamonds = {"D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "D10", "DJack", "DQueen", "DKing", "DAce"};
+
+    static String[] clubs = {"C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "CJack", "CQueen", "CKing", "CAce"};
+
+    static String[] spades = {"S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "S10", "SJack", "SQueen", "SKing", "SAce"};
+
+    static String[][] deckOfCards = {hearts, diamonds, clubs, spades};
+    static String[][] newDeck;
+
