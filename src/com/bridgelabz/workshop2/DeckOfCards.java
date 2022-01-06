203b6f06e32b0101523bd1b52c5764d6d136fba2
arpitmh25 <95077786+arpitmh25@users.noreply.github.com>
2022-01-06 18:38:50 +0530

    Adding Uc4

--- src/com/bridgelabz/workshop2/DeckOfCards.java
+++ src/com/bridgelabz/workshop2/DeckOfCards.java
@@ -1,43 +1,74 @@
+package com.bridgelabz.workshop2;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class DeckOfCards {
-    public ArrayList<String> cardsDeck = new ArrayList<>();
-    private int player;
+    public static ArrayList<String> deckCards = new ArrayList<>();
+    public static Scanner scanner = new Scanner(System.in);
+//Uc1
+    public void welcome() {
+        System.out.println("Welcome to the Deck of Card DashBoard");
+    }
 
-    public void deckOfCards() {
-
+    public void deckOfCard() {
         String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
         String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "King", "Queen", "Ace"};
         int numOfCards = suits.length * ranks.length;
         System.out.println("\nNumber of cards in the deck:" + numOfCards);
         for (int i = 0; i < ranks.length; i++) {
             for (int j = 0; j < suits.length; j++) {
-                cardsDeck.add(ranks[i] + "->" + suits[j]);
+                deckCards.add(ranks[i] + "->" + suits[j]);
             }
         }
-        toDisplay(cardsDeck);
+        toDisplay(deckCards);
     }
 
     public static void toDisplay(ArrayList<String> cardsDeck) {
+        System.out.println("\nCards in Deck:");
         for (String element : cardsDeck) {
             System.out.println(element);
         }
         System.out.println();
     }
-
+//Uc2
     public void noOfPlayers() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter no of players");
+        System.out.print("\nEnter number of players minimum 2 , maximum 4 : ");
         int player = scanner.nextInt();
         if (player >= 2 && player <= 4) {
-            System.out.println("\n"+player + "PlAYER WILL PLAY GAME");
+            System.out.println("\n" + player + " players will play the game");
         } else {
-            System.out.println("Please enter number of players");
+            System.out.println("Please enter number of players in the Range");
+            this.noOfPlayers();
+            scanner.close();
         }
     }
-
-    public void sequenceOfPlay() {
+//Uc3
+    public void seqOfPlayer(int player) {
         System.out.println("\nSequence of cards are below : ");
         for (int i = 1; i <= player; i++) {
-            System.out.println("\nPlayer " + i + " Getting card");
+            System.out.println("\nPlayer " + i + " Getting card.............");
         }
+    }
+//Uc4
+    public static void shuffleCard() {
+        System.out.println("shuffling the cards before Distribution");
+        ArrayList<String> temp = new ArrayList<String>();
+        while (!deckCards.isEmpty()) {
+            int sufCard = (int) (Math.random() * deckCards.size());
+            temp.add(deckCards.get(sufCard));
+            deckCards.remove(sufCard);
+        }
+        deckCards = temp;
+        toDisplay(deckCards);
+    }
+
+    public static void main(String[] args) {
+        DeckOfCards deckOfCardsGame = new DeckOfCards();
+        deckOfCardsGame.welcome();
+        deckOfCardsGame.deckOfCard();
+        deckOfCardsGame.noOfPlayers();
+        deckOfCardsGame.seqOfPlayer(4);
+        deckOfCardsGame.shuffleCard();
     }
 }
